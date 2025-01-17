d1f130a5341947b6db2d56e6b1f82958cce04584
кирилл гелашвилип <gelashvili@elcode.ru>
2025-01-17 23:50:31 +0300

    AddTwoNumbers and  DummyCalculator

---
+++ Leetcode/BigData3Phone/BigData3Phone.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.BigData3Phone
+{
+    //https://docs.google.com/document/d/1UG7J-zjkEa9RozFI_7aYJNfLlA_fLzuT/edit
+    //https://drive.google.com/drive/folders/1ADC6OVsLLY2YSMnnl2XoGGQWNUgWcYko?hl=ru
+    public class BigData3Phone
+    {
+        //https://docs.google.com/spreadsheets/d/1cxUjV1qm2t37L4QVdUw4c3BrsvtzAu3_/edit?gid=1278660216#gid=1278660216
+        // А таблице указаны только исходящие звонки -> можно забить на стоймость входящих звонков
+        // SMS тоже можно игнорировать
+        
+        
+
+        #region Билайн
+        public class TariffCommunicationMonster
+        {
+            public float FirstMinutePrice;
+            public float TowToNineMinutesPrice;
+            public float StartFromTenMinutesPrice;
+
+        }
+
+        public class WantToSay  
+        {
+            public float UntilSixMinutesPrice;
+            public float FromSixMinutesPrice;
+
+        }
+
+        public class MoreWords
+        {
+            public float MothPrice;
+            public int MothIncludedMinutes;
+            public float MinutesPriceAfterExpireIncludedMinutesPrice;
+        }
+        #endregion
+
+        public class ThirtyThreeCents
+        {
+            protected Dictionary<string, float> FirstMinutePrice= new Dictionary<string, float>();
+
+            public float FromSecondMinutesPrice;
+
+        }
+
+        public class ManyCalls
+        {
+            public float FromFiveToThirtyOneMinutePrice;
+            public float FromSixToThirtyMinutePrice;
+        }
+    }
+}
--- Leetcode/Program.cs
+++ Leetcode/Program.cs
@@ -1,6 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 
+using System;
+using System.Data;
 using System.Linq;
 
 
@@ -9,8 +11,8 @@ internal class Program
     private static void Main(string[] args)
     {
         var nums = new int[] { 10, 2 }; //210
-        var nums2 = new int[] { 1, 3, 30, 34, 5, 9 }; //9534330
-        //Console.WriteLine(LargestNumber(nums));
+        var nums2 = new int[] { 1, 3, 30, 34, 5, 9 }; //95343301
+        //Console.WriteLine(LargestNumber(nums2));
         //https://leetcode.com/problems/largest-number/
         //https://metanit.com/sharp/tutorial/3.23.php
         string LargestNumber(int[] nums)
@@ -31,7 +33,7 @@ internal class Program
         var intervals = new int[][] { [1, 3], [2, 6], [8, 10], [15, 18] }; //[[1,6],[8,10],[15,18]]
         var intervals2 = new int[][] { [1, 4], [4, 5] }; //[[1,5]]
         var intervals3 = new int[][] { [1, 4], [2, 3] }; //[[1,4]]
-        //Console.WriteLine(Merge(intervals3));
+        //Console.WriteLine(Merge(intervals));
         //https://leetcode.com/problems/merge-intervals/
         int[][] Merge(int[][] intervals)
         {
@@ -65,7 +67,7 @@ internal class Program
 
         var l1 = new ListNode(2, new ListNode(4, new ListNode(3)));
         var l2 = new ListNode(5, new ListNode(6, new ListNode(4)));
-        Console.WriteLine(AddTwoNumbers(l1, l2));
+        //Console.WriteLine(AddTwoNumbers(l1, l2));
         //https://leetcode.com/problems/add-two-numbers/description/
         ListNode AddTwoNumbers(ListNode l1, ListNode l2)
         {
@@ -99,6 +101,83 @@ internal class Program
             return dummyHead.next;
         }
 
+
+        //Считаем Стринги
+        //Console.WriteLine(DummyCalculator("1*4*3+1+9+2*2*5"));
+        int DummyCalculator(string input)
+        {
+
+            var value = Convert.ToInt32(new DataTable().Compute(input, null));
+
+
+            //List<string> subs = input.Split('+').OrderByDescending(x => x.Length).ToList();
+
+            //subs.Aggregate((Int32.Parse(p), Int32.Parse(x)x) => p *= x);
+
+            //var result = 0;
+            //foreach (var sub in subs)
+            //{
+            //    var subresult = 0;
+
+            //    if (sub.Length > 1)
+            //    {
+            //        subresult++;
+
+            //        var subslplit= sub.Split("*");
+
+            //        foreach (var item in subslplit)
+            //        {
+            //            subresult *= Int32.Parse(item);
+            //        }
+            //    }
+            //    else
+            //    {
+            //        result += Int32.Parse(sub);
+            //    }
+
+            //    result += subresult;
+
+            //}
+
+            return value;
+        }
+
+
+
+        //https://leetcode.com/problems/two-sum/description/
+        //Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity? +
+        var arr1 = new int[] { 2, 7, 11, 15 };
+        var target1 = 9;//[0,1]
+
+        var arr2 = new int[] { 3, 2, 4 };
+        var target2 = 6;//[1,2]
+
+        var arr3 = new int[] { 3, 3 };
+        var target3 = 6; //[0,1]
+
+        var arr4 = new int[] { 3,2,3 };
+        var target4 = 6; //[0,1]
+
+        Console.WriteLine(TwoSum(arr4,target4));
+
+        int[] TwoSum(int[] nums, int target)
+        {
+            Dictionary<int,int> hasMap= new Dictionary<int,int>();
+
+            for (int i=0;i<nums.Length;i++)
+            {
+                int tmp = nums[i];
+
+                if (hasMap.ContainsKey(target - tmp))
+                {
+                    return new int[] { i, hasMap[target - nums[i]] };
+                }
+
+                hasMap[nums[i]]=i;
+            }
+            return new int[0];
+        }
+
         Console.ReadKey();
     }
 }
@@ -114,4 +193,10 @@ public class ListNode
         this.val = val;
         this.next = next;
     }
+}
+
+public class Test
+{
+    public int val;
+
 }
