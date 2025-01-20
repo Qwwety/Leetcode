1005a2e8d0a269faf5088783122d4b800a2d0d2c
кирилл гелашвилип <gelashvili@elcode.ru>
2025-01-20 13:33:57 +0300

    New AddTwoNumbers  vars

--- Leetcode/Program.cs
+++ Leetcode/Program.cs
@@ -4,12 +4,14 @@
 using System;
 using System.Data;
 using System.Linq;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 
 internal class Program
 {
     private static void Main(string[] args)
     {
+        #region LargestNumber
         var nums = new int[] { 10, 2 }; //210
         var nums2 = new int[] { 1, 3, 30, 34, 5, 9 }; //95343301
         //Console.WriteLine(LargestNumber(nums2));
@@ -28,8 +30,9 @@ internal class Program
             return order;
 
         }
+        #endregion
 
-
+        #region Merge
         var intervals = new int[][] { [1, 3], [2, 6], [8, 10], [15, 18] }; //[[1,6],[8,10],[15,18]]
         var intervals2 = new int[][] { [1, 4], [4, 5] }; //[[1,5]]
         var intervals3 = new int[][] { [1, 4], [2, 3] }; //[[1,4]]
@@ -63,11 +66,18 @@ internal class Program
 
             return result;
         }
+        #endregion
 
+        #region AddTwoNumbers
+        var l1 = new ListNode(9, new ListNode(9, new ListNode(9,new ListNode(9,new ListNode(9, new ListNode(9, new ListNode(9)))))));
+        var l2 = new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9)))); //[8,9,9,9,0,0,0,1]
 
-        var l1 = new ListNode(2, new ListNode(4, new ListNode(3)));
-        var l2 = new ListNode(5, new ListNode(6, new ListNode(4)));
+        var l1_2 = new ListNode(2, new ListNode(4, new ListNode(3)));
+        var l2_2 = new ListNode(5, new ListNode(6, new ListNode(4))); //807
+
         //Console.WriteLine(AddTwoNumbers(l1, l2));
+        //Console.WriteLine(AddTowNumbersRecursionV1(l1_2, l2_2));
+        //Console.WriteLine(AddTowNumbersRecursionV2(l1_2, l2_2));
         //https://leetcode.com/problems/add-two-numbers/description/
         ListNode AddTwoNumbers(ListNode l1, ListNode l2)
         {
@@ -101,7 +111,57 @@ internal class Program
             return dummyHead.next;
         }
 
+        ListNode AddTowNumbersRecursionV1(ListNode l1, ListNode l2)
+        {
+            var result = AddTowNumbersMainFuncRecursionV1(l1, l2, 0, new ListNode());
 
+            return result;
+        }
+        ListNode AddTowNumbersRecursionV2(ListNode l1, ListNode l2)
+        {
+            var result = AddTowNumbersMainFuncRecursionV2(l1, l2, 0);
+
+            return result;
+        }
+
+        ListNode AddTowNumbersMainFuncRecursionV1(ListNode l1, ListNode l2, int toAdd, ListNode tree)
+        {
+            var x = l1 == null ? 0 : l1.val;
+            var y = l2 == null ? 0 : l2.val;
+
+            var sum = toAdd == 0 ? x + y : x + y + toAdd;
+
+            toAdd = sum / 10;
+
+            tree.val = sum % 10;
+
+
+            if (l1?.next != null || l2?.next != null)
+            {
+                tree.next = new ListNode();
+                AddTowNumbersMainFuncRecursion(
+                        l1.next == null ? new ListNode() : l1.next,
+                        l2.next == null ? new ListNode() : l2.next,
+                        toAdd, tree.next);
+            }
+            else if (l1?.next == null && l2?.next == null && toAdd > 0)
+            {
+                tree.next = new ListNode(toAdd);
+            }
+
+            return tree;
+        }
+        ListNode AddTowNumbersMainFuncRecursionV2(ListNode l1, ListNode l2, int toAdd)
+        {
+            if (l1 == null && l2 == null && toAdd == 0) return null;
+
+            int total = (l1 != null ? l1.val : 0) + (l2 != null ? l2.val : 0) + toAdd;
+            toAdd = total / 10;
+            return new ListNode(total % 10, AddTowNumbersMainFuncRecursionV2(l1?.next, l2?.next, toAdd));
+        }
+        #endregion
+
+        #region Считаем_Стринги
         //Считаем Стринги
         //Console.WriteLine(DummyCalculator("1*4*3+1+9+2*2*5"));
         int DummyCalculator(string input)
@@ -141,9 +201,9 @@ internal class Program
 
             return value;
         }
+        #endregion
 
-
-
+        #region TwoSum
         //https://leetcode.com/problems/two-sum/description/
         //Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity? +
         var arr1 = new int[] { 2, 7, 11, 15 };
@@ -158,8 +218,7 @@ internal class Program
         var arr4 = new int[] { 3,2,3 };
         var target4 = 6; //[0,1]
 
-        Console.WriteLine(TwoSum(arr4,target4));
-
+       // Console.WriteLine(TwoSum(arr4,target4));
         int[] TwoSum(int[] nums, int target)
         {
             Dictionary<int,int> hasMap= new Dictionary<int,int>();
@@ -177,6 +236,7 @@ internal class Program
             }
             return new int[0];
         }
+        #endregion
 
         Console.ReadKey();
     }
